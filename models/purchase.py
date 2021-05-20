0c257ac7b4fbcac6d4f8e1e5385fafd4090406f5
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2021-05-20 22:46:54 +0700

    Update purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -1,7 +1,13 @@
 from odoo import fields, models
 
+from odoo.addons.purchase.models.purchase import PurchaseOrder as Purchase
+
 class PurchaseOrder(models.Model):
 	_inherit = 'purchase.order'
 
+	@api.model
+	def _default_picking_type(self):
+		return self._get_picking_type(self.env.context.get('company_id') or self.env.company.id)
+		
 	picking_type_id = fields.Many2one('stock.picking.type', 'Deliver To', states=Purchase.READONLY_STATES, required=True, default=_default_picking_type, domain="['|', ('warehouse_id', '=', False), ('warehouse_id.company_id', '=', company_id)]",
 		help="This will determine operation type of incoming shipment")
