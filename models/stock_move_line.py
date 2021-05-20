43e1a2df7cf5fa167a56d9f1d3a5eba8dffa6ddd
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2021-05-20 23:32:49 +0700

    Update stock_move_line.py

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class StockMoveLine(models.Model):
 	_inherit = "stock.move.line"
 
-	line_notes = fields.Text(string="Additional Desc")
+	line_notes = fields.Char(string="Additional Desc")
