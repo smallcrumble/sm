54699b723f9638052dfba4ceb6c4bf125f87f25d
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2021-05-27 22:20:39 +0700

    Update stock_move.py

--- models/stock_move.py
+++ models/stock_move.py
@@ -4,3 +4,8 @@ class StockMove(models.Model):
 	_inherit = "stock.move"
 
 	line_notes = fields.Char(string="Additional Desc")
+
+class StockMoveLine(models.Model):
+	_inherit = "stock.move.line"
+
+	line_notes = fields.Char(related="move_id.line_notes", string="Additional Desc")
