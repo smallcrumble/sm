db8cabdf7e5d06a9904aa874eb22377225c736c3
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2021-05-31 22:41:29 +0700

    invisible

--- models/account_move.py
+++ models/account_move.py
@@ -3,5 +3,4 @@ from odoo import fields, models
 class AccountMove(models.Model):
 	_inherit = "account.move"
 
-	#ga jadi
-	#ref = fields.Char(string='Reference', copy=False, tracking=True, required=True)
+	ref = fields.Char(string='Reference', copy=False, tracking=True, required=True)
