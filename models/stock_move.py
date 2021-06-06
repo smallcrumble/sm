ee44bcf359961a8314b1076d1095668fd0e6308e
smallcrumble <75667668+smallcrumble@users.noreply.github.com>
2021-06-06 22:26:32 +0700

    Update stock_move.py

--- models/stock_move.py
+++ models/stock_move.py
@@ -5,6 +5,19 @@ class StockMove(models.Model):
 
 	line_notes = fields.Char(string="Detail Notes")
 
+	def _action_done(self, cancel_backorder=False):
+		res = super(StockMove,self)._action_done(cancel_backorder)
+		print('po line id : '+ str(res['purchase_line_id'].id))
+		print('line notes s.move : '+ str(res['line_notes']))
+		#res['purchase_line_id'].line_notes = res['line_notes']
+		
+		return res
+
+	def _prepare_account_move_line(self, move=False):
+		res = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
+		res['line_notes']=self.line_notes
+		return res
+
 class StockMoveLine(models.Model):
 	_inherit = "stock.move.line"
 
